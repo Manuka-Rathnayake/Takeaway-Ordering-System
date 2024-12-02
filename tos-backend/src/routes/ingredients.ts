ddcd9c92837d2cdeda7101d90766d0e227aea7b7
antiloger <antilogerme@gmail.com>
2024-12-02 12:35:16 +0530

    setup ws, setup roleauth

--- tos-backend/src/routes/ingredients.ts
+++ tos-backend/src/routes/ingredients.ts
@@ -1,15 +1,2 @@
-import { Request, Response } from "express";
-
-export const Login = async (req: Request, res: Response) => {
-  try {
-
-    return res.status(201).json({ msg: `` })
-  } catch (e) {
-    console.log(e)
-    return res.status(500).json({ msg: "Internal Server" })
-
-  }
-
-}
 
 
